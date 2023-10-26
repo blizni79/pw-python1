5909a96d9dd1a3fd6abdd34c30271b32c72149b0
Jakub Chmielak <jakub.chmielak@pw.edu.pl>
2023-10-26 08:38:07 +0200

    Add

---
+++ c1.py
@@ -0,0 +1,9 @@
+# Instrukcja warunkowa if-else:
+# Poproś użytkownika o podanie liczby i sprawdź, czy jest ona większa niż 10. Wypisz odpowiedni komunikat.
+
+liczba = int(input("Podaj liczbę: "))
+if liczba > 10:
+    print("Liczba jest większa niż 10.")
+else:
+    print("Liczba nie jest większa niż 10.")
+
